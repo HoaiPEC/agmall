7e3e9761add8954c86fd48932ea3f260f8754c5b
Hoai <hoainxptit@gmail.com>
2020-09-21 17:37:38 +0700

    Template module admin

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+mix.styles('Modules/Admin/Resources/assets/css/app.css', 'public/css/app.css');
